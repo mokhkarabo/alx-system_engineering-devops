7a4a03cadaccffaa4a65bdf2753c549e794d0097
mokhkarabo <mokherere.karabo@gmail.com>
2024-04-19 18:52:29 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs puppet-lint
-package { 'puppet-lint':
+#bin/user//bin/pup
+# Installs a specific of flask  {2.1.0}
+package { 'flask':
   ensure   => '2.5.0',
-  provider => gem,
+  provider => 'pip'
 }
