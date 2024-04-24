cd92859c6e49659596dff72816158ae51580c915
mokhkarabo <mokherere.karabo@gmail.com>
2024-04-23 19:23:37 -0700

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#bin/user//bin/pup
-# Installs a specific of flask  {2.1.0}
+# Install flask
 package { 'flask':
-  ensure   => '2.5.0',
-  provider => 'pip'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
