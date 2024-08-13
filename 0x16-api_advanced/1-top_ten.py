7a96df2c5d2327ba224235412158ed623f46c0a8
mokhkarabo <mokherere.karabo@gmail.com>
2024-08-13 14:05:10 -0700

    api-advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts listed for a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
-    # Define headers for the HTTP request, including User-Agent
-    headers = {
-        "User-Agent": "python:subreddit.topten:v1.0.0 (by /u/yourusername)"
-    }
-    
-    # Define parameters for the request, limiting the number of posts to 10
-    params = {
-        "limit": 10
-    }
-    
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        # Send a GET request to the subreddit's hot posts page
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        
-        # Check if the response status code indicates success
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get("data", {}).get("children", [])
-            
-            if not posts:
-                print("None")
-                return
-            
-            for post in posts:
-                print(post.get("data", {}).get("title", "No Title"))
-        
-        else:
-            # Print None for any non-successful status codes (e.g., 404 for invalid subreddit)
-            print("None")
-    
-    except requests.RequestException:
-        # Handle any request exceptions and print None
-        print("None")
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
